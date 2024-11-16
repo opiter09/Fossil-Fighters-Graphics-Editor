245fa3b8bccfa5a33cad25531566d7d6fb73bc14
opiter09 <92452568+opiter09@users.noreply.github.com>
2024-11-15 22:59:24 -0500

    anti-piracy

--- start.py
+++ start.py
@@ -20,6 +20,13 @@ if (os.path.exists(folder) == False):
     subprocess.run([ "dslazy.bat", "UNPACK", sys.argv[1] ])
     # os.rename("NDS_UNPACK", folder)
     check = 1
+    
+if (rom == "ffc"):
+    subprocess.run([ "xdelta3-3.0.11-x86_64.exe", "-d", "-f", "-s", "NDS_UNPACK/arm9.bin", "ffc_apFix.xdelta",
+        "NDS_UNPACK/arm9x.bin" ])
+    if (os.path.exists("NDS_UNPACK/arm9x.bin") == True):
+        os.remove("NDS_UNPACK/arm9.bin")
+        os.rename("NDS_UNPACK/arm9x.bin", "NDS_UNPACK/arm9.bin")
 
 if (os.path.exists("motion_" + folder) == False):
     os.mkdir("motion_" + folder)
